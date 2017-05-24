64d243544fdf4fab167ea8c58e721d7bf530b091
Romano <codyromano@gmail.com>
2017-05-24 11:59:47 -0700

    Db object

---
+++ src/public/Db.js
@@ -0,0 +1,12 @@
+var Db = {
+  //objectFlag: '_flag',
+  saveObject: function(key, value) {
+    localStorage.setItem()
+  },
+  save: function(key, value) {
+    localStorage.setItem(key, value);
+  },
+  get: function(key) {
+    return localStorage.getItem(key);
+  }
+};
