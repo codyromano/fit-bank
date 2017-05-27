f50dd5b36229ed0811f1e6ba87d5f23bdbbccadc
Cody Romano <codyromano@gmail.com>
2017-05-27 10:38:25 -0700

    Add Save feature

--- src/public/Balance.js
+++ src/public/Balance.js
@@ -5,7 +5,7 @@ var Balance = {
   maxDebt: -(Reward.getRewardAmount() * 2),
   add: function(amount) {
     var current = parseFloat(this.get()); 
-    this.set(current + amount); 
+    this.set(current + parseFloat(amount)); 
   },
   deduct: function(amount) {
     var current = parseFloat(this.get()),
@@ -18,7 +18,27 @@ var Balance = {
   },
   get: function() {
     var amount = Db.get('balance') || 0; 
-    return amount; 
+    return parseFloat(amount); 
+  },
+  getSavings() {
+    const amount = Db.get('savings') || 0;
+    return parseFloat(amount);
+  },
+  deductSavings(amount) {
+    const saved = this.getSavings();
+    if (amount <= saved && saved - amount >= 0) {
+      Db.save('savings', saved - amount);
+      return true;
+    }
+    return false;
+  },
+  addSavings(amount) {
+    if (!isNaN(amount) && amount <= this.get()) {
+      const savings = this.getSavings();
+      Db.save('savings', savings + amount);
+      return true;
+    }
+    return false;
   },
   addCommas: function(number) {
     return (number + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
