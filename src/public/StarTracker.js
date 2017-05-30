4948f088f36959b08c6274c618c3fcc457b4a5dc
Cody Romano <codyromano@gmail.com>
2017-05-29 19:17:53 -0700

    StarTracker class with unlock method

--- src/public/StarTracker.js
+++ src/public/StarTracker.js
@@ -16,5 +16,49 @@ const stars = [
     about: 'Spend calories on a meal or snack',
     locked: true,
     seenAfterUnlock: false
+  },
+  {
+    id: 'first-save',
+    about: 'Save calories for later',
+    locked: true,
+    seenAfterUnlock: false
   }
 ];
+
+const clone = obj => Object.assign({}, obj);
+
+class StarTracker {
+  constructor(stars = []) {
+    this.storageKey = 'stars';
+    this.stars = Db.get(this.storageKey) || stars;
+    console.assert(Array.isArray(this.stars));
+  }
+  onStarListChange() {
+    Db.save(this.storageKey, this.stars);
+    // TODO: Publish update 
+  }
+  getStars() {
+    return Object.assign([], this.stars.map(clone));
+  }
+  getStarById(id) {
+    const match = this.stars.filter(star =>
+      star.id === id);
+    return match.length ? match[0] : null;
+  }
+  getStarsUnseenAfterUnlock() {
+  }
+  markSeen(starId) {
+  }
+  unlock(starId) {
+    const star = this.getStarById(starId);
+    if (star) {
+      star.locked = false;
+      this.onStarListChange();
+      return true;
+    } else {
+      throw new Error(`There's no star with id ${starId}.`);
+    }
+  }
+}
+
+const starTracker = new StarTracker(stars);
