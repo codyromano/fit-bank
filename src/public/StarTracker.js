80923b8f373f344a213a3e3d235bb013e37a814b
Cody Romano <codyromano@gmail.com>
2017-05-29 19:03:26 -0700

    Source achievements from array

---
+++ src/public/StarTracker.js
@@ -0,0 +1,20 @@
+const stars = [
+  {
+    id: 'first-login',
+    about: 'Login for the first time',
+    locked: true,
+    seenAfterUnlock: false
+  },
+  {
+    id: 'first-deposit',
+    about: 'Deposit calories',
+    locked: true,
+    seenAfterUnlock: false
+  },
+  {
+    id: 'first-spend',
+    about: 'Spend calories on a meal or snack',
+    locked: true,
+    seenAfterUnlock: false
+  }
+];
