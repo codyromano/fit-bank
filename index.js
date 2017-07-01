6cf60f1b2862e603819ef21bd1d6ecac97d1a310
Cody Romano <codyromano@gmail.com>
2017-06-30 23:51:57 -0700

    Refactor server for compatbility with proxy

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const appBasePath = '/fit-bank/';
 const SERVER_PORT = 3456;
 
-var express = require('express');
-var app = express();
-
-// Static directory
-app.use(express.static(__dirname + '/src/public'));
-
-// Rendering views
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-app.get('/', function(req, res) {
-  res.render('index'); 
-});
+const publicDir = path.join(__dirname, 'src/public');
+app.use(appBasePath, express.static(publicDir));
 
 app.listen(SERVER_PORT, function() {
   console.log('fit-bank listening on port %s!', SERVER_PORT);
